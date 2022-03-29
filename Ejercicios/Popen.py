f6747b0d8eb7ab3a0949cd9d289de872b029b4ff
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-03-29 19:53:53 -0300

    Trabajo actualizado

--- Ejercicios/Popen.py
+++ Ejercicios/Popen.py
@@ -34,7 +34,7 @@ def comando(args):
             msg_1 = (now + f": Comando {args.c} ejecutado correctamente")
             with open(f"/home/nk-nicolas/Documentos/Computacion-II/Ejercicios/{args.f}.txt", "w") as fd:
                 fd.write(str(out))
-            with open (f"/home/nk-nicolas/Documentos/Computacion-II/Ejercicios/{args.l}.txt", "w") as ld:
+            with open(f"/home/nk-nicolas/Documentos/Computacion-II/Ejercicios/{args.l}.txt", "w") as ld:
                 ld.write(str(msg_1))
 
         else:
@@ -42,13 +42,5 @@ def comando(args):
                 ld.write(str(err))
 
 
-
-
-
 if __name__ == '__main__':
     main()
-
-
-
-
-
