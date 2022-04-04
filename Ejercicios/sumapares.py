d1bb42355bc7bf03dab25060f6815e0634c92019
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-04-03 21:20:13 -0300

    TP 3 terminado

--- Ejercicios/sumapares.py
+++ Ejercicios/sumapares.py
@@ -5,37 +5,43 @@ def main():
     suma = 0
     parser = argparse.ArgumentParser(description="Linea de comandos")
     parser = argparse.ArgumentParser(add_help=False)
+    group = parser.add_mutually_exclusive_group()
     parser.add_argument("-n", type=int, help="Elegir numero")
-    parser.add_argument("-h", type=str, help="Mostrar ayuda de uso")
-    parser.add_argument("-v", type=str, help="Activar modo verbose")
+    group.add_argument("-he", action='store_true', help="Activa ayuda de uso")
+    group.add_argument("-v", action='store_true', help="Activa modo verbose")
     args = parser.parse_args()
-    if args.h:
-        help_usage() 
-    execute(args, suma)
+    if args.he:
+        help_usage(args)
+    else:
+        print("Ejecutado sin ayuda.\n")
+    if args.n:
+        execute(args, suma)
         
 
 def execute(args, suma):
     # ejecuto fork en un for, en rango de la cantidad indicada en -n 
     for i in range(args.n):
-        retorno = os.fork()    
-    pid = os.getpid()
-    ppid = os.getppid()
-    print('PID = %d , PPID = %d , Retorno: %d' % (pid, ppid, retorno))
-    time.sleep(1)
-    # uso un if para verificar si es el proceso hijo o el padre
-    # aparece el hijo
-    if retorno == 0:
-        # chequea si se ejecuto con modo verbose
-        if args.v:
-            mode_verbose(pid, ppid, suma)
-        # si no se ejecuto el -v ejecuta la suma de modo normal
-        else:
-            suma_pares(pid,ppid, suma)
+        retorno = os.fork()
+        pid = os.getpid()
+        ppid = os.getppid()
+        # uso un if para verificar si es el proceso hijo o el padre
+        # aparece el hijo
+        if retorno == 0:
+            # chequea si se ejecuto con modo verbose
+            if args.v:
+                mode_verbose(pid, ppid, suma)
+                os._exit(0)
+            # si no se ejecuto el -v ejecuta la suma de modo normal
+            else:
+                suma_pares(pid,ppid, suma)
+                os._exit(0)
     # aparece el padre
-    else: 
-        os.wait()
-        print('Soy el padre')
-        print('El proceso de suma ha terminado')
+    time.sleep(3)
+    os.wait()
+    print('El proceso de suma ha terminado')
+            
+            
+    
   
 
 def suma_pares(pid, ppid, suma):
@@ -45,8 +51,8 @@ def suma_pares(pid, ppid, suma):
     print(f'{pid} - {ppid}:', suma)      
         
 
-def help_usage():
-    print('\nHELP:\n \nDebera pasarle un argumento de numero al programa para que se ejecute: \n'
+def help_usage(args):
+    print('\nAYUDA:\n \nDebera pasarle un argumento de numero al programa para que se ejecute: \n'
           '\nEjemplo: \n./sumapares.py -n 10\n \nDonde 10 es el numero indicado de procesos hijos '
           'que van a crearse. \nEl sistema mostrara los 10 procesos hijos con sus respectivas '
           'sumas de pares.\n')
