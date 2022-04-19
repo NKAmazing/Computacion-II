aeec2a67385edfe35128d2704b136496fa61e4f4
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-04-18 22:05:03 -0300

    comentado y ordenado

--- Ejercicios/inversor.py
+++ Ejercicios/inversor.py
@@ -28,38 +28,42 @@ def create_file(args, text):
 def execute_fork(fd):
     # leo las lineas del texto
     lines = fd.readlines()
-    # print(len(lines))
-
+    
+    # establezco dos listas, una de lectura y otra de escritura
     list_read = []
     list_write = []
 
-    # ejecuto un fork dentro de un for, en rango de la cantidad de lineas de mi fd
+    # utlizo un for para operar pipe y fork segun la cantidad de lineas de mi archivo
     for i in range(len(lines)):
+        # uso el pipe
         r, w = os.pipe()
+        # cada r se agrega a la lista
         list_read.append(r)
+        # cada w se agrega a la lista
         list_write.append(w)
+        # utilizo fork
         ret = os.fork()
 
         # aparece el hijo
         if ret == 0:
-            read_write(list_read[i])
-            # read = os.read(r, 100)
-            # print("Hijos leyendo: \n", read.decode())
+            # el hijo llama a la funcion read_line que leera una linea del archivo
+            read_line(list_read[i])
             os._exit(0)
     
-    # aparece el padre
+    # aparece el padre y escribe el archivo en un pipe
     time.sleep(1)
     for i in range(len(lines)):
         line = lines[i]
+        # lee la lista donde esta almacenado cada w, en posicion de i, y luego a cada linea la invierte
         os.write(list_write[i], bytes(line[::-1].encode()))
         time.sleep(1)
-        # os.write(list_write[i], str.encode(line))
 
     for i in range(len(lines)):
         os.wait()
 
-def read_write(r):
+def read_line(r):
     time.sleep(1)
+    # el hijo lee la linea que mando el padre
     read = os.read(r, 100)
     print(read.decode())
     
