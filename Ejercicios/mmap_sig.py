da2bb55386da20a34c8d9e2dfb43ba0aac03f6c1
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-05-03 23:43:18 -0300

    mmap_sig terminado

--- Ejercicios/mmap_sig.py
+++ Ejercicios/mmap_sig.py
@@ -16,6 +16,7 @@ def main():
 
 def handler_parent(s, f):
     if s == signal.SIGUSR1:
+        # usamos un seek para volver al principio y leemos
         print("rebobinando...")
         memory.seek(0)
         print("leyendo desde la memoria - ", memory.tell())
@@ -23,6 +24,7 @@ def handler_parent(s, f):
         print("Padre leyendo: ", read.decode())
         os.kill(list_pid[1], signal.SIGUSR1)
     elif s == signal.SIGUSR2:
+        # manda señal al hijo 2, espera, y luego sale
         os.kill(list_pid[1], signal.SIGUSR2)
         os.wait()
         print("Padre saliendo")
@@ -31,11 +33,10 @@ def handler_parent(s, f):
 
 def handler_child(s, f):
     if s == signal.SIGUSR1:
+        # recibe la señal, lee la memoria y guarda en el archivo
         print("Hijo 2 notificado...")
-        # memory.seek(0)
-        # read = memory.read(filesize).decode().upper()
         read = memory.readline().decode().upper()
-        print(type(read))
+        # print(type(read))
         if not os.path.exists(args.f):
             fd = open(f"{args.f}", "w")
             fd.close
@@ -43,6 +44,7 @@ def handler_child(s, f):
             fd.write(read.strip("\00"))
             fd.flush()
     elif s == signal.SIGUSR2:
+        # recibe la otra señal y sale
         print("Hijo 2 saliendo")
         sys.exit(0)
 
@@ -53,25 +55,33 @@ def execute(args):
     global memory, filesize
     filesize = 1024
     memory = mmap.mmap(-1, filesize)
-    print("padre esperando...")
-
+    print("padre esperando...\n")
+    # forkeo dos veces
     for process in range(2):
         # utilizo fork
         global ret
         ret = os.fork()
+        # itero con un if, si es el hijo, agrego a la lista el pid
         if ret == 0:
             list_pid.append(os.getpid())
+            # hijo 1
             if process == 0:
+                # recorro con un for el sys.stdin para pasar datos por terminal
                 for line in sys.stdin:
+                    # si line es 'bye', manda una señal al padre y sale
                     if line[:3] == "bye":
+                        time.sleep(1)
                         os.kill(os.getppid(), signal.SIGUSR2)
                         print("Hijo 1 saliendo")
                         sys.exit(0)
+                    # el hijo escribe en la memoria y manda una señal al padre
                     print("Hijo 1 escribiendo en la memoria - ", memory.tell())
                     memory.write(bytes(line.encode()))
+                    print("Hijo 1 terminado de escribir la linea - ", memory.tell())
                     os.kill(os.getppid(), signal.SIGUSR1)
+            # hijo 2
             else:
-                
+                # recibe las señales del padre
                 signal.signal(signal.SIGUSR1, handler_child)
                 signal.signal(signal.SIGUSR2, handler_child)
                 while True:
@@ -81,14 +91,9 @@ def execute(args):
             
         
     # padre
-    print(list_pid)
+    signal.signal(signal.SIGUSR2, handler_parent)
     signal.signal(signal.SIGUSR1, handler_parent)
     signal.pause()
-
-    signal.signal(signal.SIGUSR2, handler_parent)
-    signal.pause()
-    # print("Padre saliendo")
-
 
     os.wait()
 
