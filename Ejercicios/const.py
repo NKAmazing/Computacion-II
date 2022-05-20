a6cad4fd05229d111f92534bc565104e57523803
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-05-20 03:00:53 -0300

    constantes organizadas en un python file aparte

---
+++ Ejercicios/const.py
@@ -0,0 +1,40 @@
+# CONSTANTS
+
+# data
+MATRIX = '''
+1, 2, 3
+
+4, 5, 6
+'''
+
+# errors - out of range - empty messages
+ARGS_ERR = "Need more arguments to execute the program."
+UNK_OP = "ERROR. Unknown operation."
+
+# messages
+SQP_MSG = "calculating the square power of: "
+SQRT_MSG = "calculating the square root of: "
+LOG_MSG = "calculating the logarithm of: "
+PS_PID_MSG = "Process"
+RESULT_MSG = "Result of the operation: "
+
+
+# titles
+OP_TITLE = "Solving operation: "
+
+
+# argparse messages
+POOL_ARG_DESCR = "Command line of Pool Example."
+PROC_ARG_HELP = "Indicates number of process."
+PATH_ARG_HELP = "Indicates path of the file."
+OP_ARG_HELP = '''Indicates calculation function.
+                    - pot (Square Power)
+                    - raiz (Square Root)
+                    - log (Logarithm)
+'''
+
+# help messages
+HELP_MSG = "To more details, use -h or --help to receive more help."
+
+# operators
+JUMP_LINE = "\n"
