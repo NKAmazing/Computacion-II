547868f17424441e43375f88155ffce7b6f9e2da
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-06-04 13:58:08 -0300

    terminado, con mec ipc queue

--- Ejercicios/th_rot13.py
+++ Ejercicios/th_rot13.py
@@ -8,48 +8,62 @@ import time
 def main():
     q = queue.Queue()
     s = threading.BoundedSemaphore(1)
+    q2 = queue.Queue()
     threads = []
-    threads.append(threading.Thread(target=thread_function, args=(s, q), daemon=True))
-    threads.append(threading.Thread(target=thread_2_function, args=(s, q), daemon=True))
+    threads.append(threading.Thread(target=thread_function, args=(s, q, q2), daemon=True))
+    threads.append(threading.Thread(target=thread_2_function, args=(s, q, q2), daemon=True))
     for thread in threads:  # Starting all the threads
         thread.start()
     for thread in threads:  # Waits for threads to complete before moving on with the main script.
         thread.join()
     q.join()
+    q2.join()
     print(cs.END_MSG)
 
-def thread_function(s, q):
+def thread_function(s, q, q2):
     s.acquire()
-    global items
-    items = []
+    print(cs.INPUT_MSG)
+    print(cs.JUMP_LINE)
     for line in sys.stdin:
         if line[:5] == cs.ARG_STR:
             break
-        print(f"{cs.L_ADDING_MSG} {line}")
-        items.append(line)
+        print(f"{cs.Q_ADDING_MSG} {line}")
+        q2.put(line.strip("\n"))
         time.sleep(1)
-    print(items)
     s.release()
     time.sleep(1)
     s.acquire()
     while True:
         print(cs.READ_MSG)
+        time.sleep(0.5)
+        print(cs.JUMP_LINE)
         item = q.get()
-        print(f"{cs.THREAD_MSG} {item}")
-        print(f"{cs.THREAD_MSG_2} {item}")
+        print(f"{cs.THREAD_MSG_3} '{item}'")
         q.task_done()
+        print(cs.JUMP_LINE)
+        time.sleep(1)
         if q.empty():
             break
     s.release()
             
-def thread_2_function(s, q):
+def thread_2_function(s, q, q2):
     s.acquire()
-    for item in items:
+    print(cs.JUMP_LINE)
+    while True:
+        print(cs.READ_MSG)
+        time.sleep(0.5)
+        print(cs.JUMP_LINE)
+        item = q2.get()
         print(f"{cs.PUT_Q_MSG} {item}")
         item = codecs.encode(item, cs.ARG_ROT_13)
         q.put(item)
+        q2.task_done()
+        print(cs.JUMP_LINE)
         time.sleep(1)
+        if q2.empty():
+            break
     s.release()
+    
 
 if __name__ == '__main__':
     main()
