4126d5a4aad11f83e3914a5a26cea6aa6e121b7e
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-06-04 12:52:54 -0300

    arreglado error queue infinita

--- Ejercicios/th_rot13.py
+++ Ejercicios/th_rot13.py
@@ -11,9 +11,9 @@ def main():
     threads = []
     threads.append(threading.Thread(target=thread_function, args=(s, q), daemon=True))
     threads.append(threading.Thread(target=thread_2_function, args=(s, q), daemon=True))
-    for thread in threads:
+    for thread in threads:  # Starting all the threads
         thread.start()
-    for thread in threads:
+    for thread in threads:  # Waits for threads to complete before moving on with the main script.
         thread.join()
     q.join()
     print(cs.END_MSG)
@@ -23,13 +23,15 @@ def thread_function(s, q):
     global items
     items = []
     for line in sys.stdin:
-        if line[:5] == 'break':
+        if line[:5] == cs.ARG_STR:
             break
         print(f"{cs.L_ADDING_MSG} {line}")
         items.append(line)
         time.sleep(1)
+    print(items)
     s.release()
     time.sleep(1)
+    s.acquire()
     while True:
         print(cs.READ_MSG)
         item = q.get()
@@ -38,12 +40,13 @@ def thread_function(s, q):
         q.task_done()
         if q.empty():
             break
+    s.release()
             
 def thread_2_function(s, q):
     s.acquire()
     for item in items:
         print(f"{cs.PUT_Q_MSG} {item}")
-        item = codecs.encode(item, 'rot13')
+        item = codecs.encode(item, cs.ARG_ROT_13)
         q.put(item)
         time.sleep(1)
     s.release()
